45862e484e59860b71fdc7489e264d1b17b6c4c0
koujianfeng <koujianfeng@do-global.com>
2023-12-07 16:05:13 +0800

    first: 刚刚创建了个vue3的ts项目

---
+++ README.md
@@ -0,0 +1,27 @@
+# ks-test
+
+- Vue3 + ts 项目
+
+## 开发
+
+```sh
+npm install
+```
+
+```sh
+npm run dev
+```
+
+```sh
+npm run build
+```
+
+### 美化代码
+
+```sh
+npm run format
+```
+
+## Vite 文档
+
+See [Vite Configuration Reference](https://vitejs.dev/config/).
---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>
+      <RouterView />
+    </div>
+
+    <div>
+      <div>
+        <RouterLink to="/">Home</RouterLink>
+      </div>
+
+      <div>
+        <RouterLink to="/about">About</RouterLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped></style>
